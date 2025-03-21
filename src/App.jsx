02bd34f6314c354ad3e77a094ab9265fbcb2187c
Tytan Telford <tytantelford@dpadmins-mbp.lan>
2025-03-21 15:08:00 -0600

    final

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Class extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello 1</h1>
+        <h1>hello 2</h1>
+      </div>
+    );
+  }
+}
+
+export default Class;
